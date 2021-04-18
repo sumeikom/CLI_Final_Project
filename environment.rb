7e2438f360cff2cdcfca4a05af81337b9952b1b2
Sumei Kom <sumeikom@gmail.com>
2021-04-18 09:43:21 -0400

    cleaned up enivroment

--- environment.rb
+++ environment.rb
@@ -3,12 +3,4 @@ require "pry"
 require "bundler"
 Bundler.require
 
-#  require_all './CLI_project_studio_ghibli'
-
  require_rel './lib'
-
-# require_relative './lib/api.rb' 
-# require_relative './lib/cli.rb'
-# require_relative './lib/films.rb'
-
-# binding.pry
